fc478fffac4b006836abc4bce3eeb2df8278c4ae
alessio <alessio@pop-os.localdomain>
2023-04-25 17:05:05 +0200

    Added script for automation

---
+++ notification/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Insert your regitry name: " registry_name
+image_name=$registry_name/notification
+
+echo "*** Generating JavaScript ***"
+npm run install
+npm run build
+
+echo "*** Generating and pushing Docker image ***"
+docker build -t $image_name .
+docker push $image_name
